1849a441344f8afcb419ac262298460e5c15c4e6
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2022-02-21 14:05:51 +0200

    Perusta

---
+++ Kayttoliittyma.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Kayttoliittyma {
+    private Scanner lukija;
+
+
+    public Kayttoliittyma(Scanner lukija) {
+        this.lukija = lukija;
+    }
+
+    public void kaynnista() {
+        
+    }
+}
