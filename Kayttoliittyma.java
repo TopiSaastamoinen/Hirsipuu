d4413947812ca1d1cce264981d0a5ffb84b1d775
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2022-02-22 10:16:52 +0200

    Hirsipuu korjaus, käyttöliittymän rakennus

--- Kayttoliittyma.java
+++ Kayttoliittyma.java
@@ -1,14 +1,36 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Kayttoliittyma {
     private Scanner lukija;
+    private ArrayList<String> sanalista;
 
 
     public Kayttoliittyma(Scanner lukija) {
         this.lukija = lukija;
+        this.sanalista = new ArrayList<>();
     }
 
     public void kaynnista() {
-        
+        try (Scanner tiedostonlukija = new Scanner(Paths.get("sanat.txt"))) {
+            while (tiedostonlukija.hasNextLine()) {
+                String rivi = tiedostonlukija.nextLine();
+                if (rivi.isEmpty()) {
+                    break;
+                }
+                sanalista.add(rivi);
+            }
+        } catch (Exception e) {
+            System.out.println("Virhe!" + e.getMessage()); //TODO: handle exception
+        }
+
+        while (true) {
+            
+            
+        }
     }
+
+
+
 }
