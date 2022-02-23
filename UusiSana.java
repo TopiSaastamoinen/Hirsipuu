43492956fea5bc43eb563aec560ece312971d5f4
ioyli <90900819+ioyli@users.noreply.github.com>
2022-02-23 21:28:58 +0200

    UusiSana -olion runko

--- UusiSana.java
+++ UusiSana.java
@@ -1,5 +1,22 @@
-
+import java.util.Scanner;
 
 public class UusiSana {
+    private Scanner lukija;
 
+    public UusiSana() {
+        this.lukija = new Scanner(System.in);
+    }
+
+    public void lisaa() {
+        System.out.println("Syötä lisättävä sana: ");
+        String syote = lukija.nextLine().toUpperCase();
+
+        // tarkista kaikki sanan kirjaimet, jos ääkkösiä => virhe
+        if (!(! syote.matches(".*[^A-Z].*"))) {
+            System.out.println("Sanassa ei voi olla ääkkösiä!");
+        } else {
+            // lisää syöte sanat.txt tiedostoon
+            // käytä lisättyä sanaa heti?
+        }
+    }
 }
